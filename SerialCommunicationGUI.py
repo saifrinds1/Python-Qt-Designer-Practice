69873d263630c23f6fe26895b153f71b50f41879
saifrinds1 <saifrinds1@gmail.com>
2018-01-10 13:19:57 +0500

    In this version  i changed the GUI and it can send data to serial port but there is no functionality for read from serialPort

--- SerialCommunicationGUI.py
+++ SerialCommunicationGUI.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'MyFirstGUI.ui'
+# Form implementation generated from reading ui file 'SerialCommunicationGUI.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
@@ -12,39 +12,51 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.setWindowModality(QtCore.Qt.NonModal)
-        MainWindow.resize(391, 289)
+        MainWindow.resize(210, 242)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setWindowTitle("")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("../../Pictures/serial-port-icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         MainWindow.setAutoFillBackground(False)
         MainWindow.setIconSize(QtCore.QSize(96, 48))
+        MainWindow.setDocumentMode(False)
         MainWindow.setDockNestingEnabled(False)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.btnConnect = QtWidgets.QPushButton(self.centralwidget)
-        self.btnConnect.setGeometry(QtCore.QRect(12, 9, 75, 23))
+        self.btnConnect.setGeometry(QtCore.QRect(135, 5, 70, 25))
+        self.btnConnect.setIconSize(QtCore.QSize(0, 0))
         self.btnConnect.setObjectName("btnConnect")
-        self.etxtSerialPortName = QtWidgets.QLineEdit(self.centralwidget)
-        self.etxtSerialPortName.setGeometry(QtCore.QRect(92, 10, 113, 20))
-        self.etxtSerialPortName.setInputMask("")
-        self.etxtSerialPortName.setText("")
-        self.etxtSerialPortName.setEchoMode(QtWidgets.QLineEdit.NoEcho)
-        self.etxtSerialPortName.setObjectName("etxtSerialPortName")
-        self.listView = QtWidgets.QListView(self.centralwidget)
-        self.listView.setGeometry(QtCore.QRect(10, 40, 371, 191))
-        self.listView.setObjectName("listView")
         self.btnSend = QtWidgets.QPushButton(self.centralwidget)
-        self.btnSend.setGeometry(QtCore.QRect(307, 239, 75, 23))
+        self.btnSend.setGeometry(QtCore.QRect(135, 190, 70, 25))
         self.btnSend.setObjectName("btnSend")
         self.etxtMsg = QtWidgets.QLineEdit(self.centralwidget)
-        self.etxtMsg.setGeometry(QtCore.QRect(10, 240, 291, 20))
+        self.etxtMsg.setGeometry(QtCore.QRect(5, 190, 125, 25))
         self.etxtMsg.setInputMask("")
         self.etxtMsg.setText("")
-        self.etxtMsg.setEchoMode(QtWidgets.QLineEdit.NoEcho)
+        self.etxtMsg.setEchoMode(QtWidgets.QLineEdit.Normal)
         self.etxtMsg.setObjectName("etxtMsg")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(5, 35, 200, 150))
+        self.listWidget.setObjectName("listWidget")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(5, 5, 90, 25))
+        self.comboBox.setObjectName("comboBox")
+        self.refresh = QtWidgets.QPushButton(self.centralwidget)
+        self.refresh.setGeometry(QtCore.QRect(100, 5, 30, 25))
+        self.refresh.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("../../../Pictures/refresh.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.refresh.setIcon(icon1)
+        self.refresh.setObjectName("refresh")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 391, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 210, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -58,13 +70,11 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.btnConnect.setText(_translate("MainWindow", "Connect"))
         self.btnSend.setText(_translate("MainWindow", "Send"))
         self.actionSEND.setText(_translate("MainWindow", "SEND"))
 
-
-"""[summary]
+"""
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -73,5 +83,4 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
 """
--- serialCommunication.py
+++ serialCommunication.py
@@ -1,22 +1,102 @@
-import MyFirstGUI
-from PyQt5 import QtCore, QtGui, QtWidgets
+import SerialCommunicationGUI
+from PyQt5 import QtCore, QtGui, QtWidgets, QtSerialPort
 import sys
+import glob
+import serial
+
+#serialPort Initialize 
+serialPort = serial.Serial()
+serialPort.baudrate = 9600
+
+#Return available serialPorts
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+#Connect Button Function
+def ConnectButtonClicked():
+    if ConnectWithPort(serialPort, MainWindowGUI.comboBox.currentText()):
+        print("Connected")
+        MainWindowGUI.btnSend.setEnabled(True)
+        MainWindowGUI.etxtMsg.setEnabled(True)
+    else:
+        print("Could Not Connected")
+        MainWindowGUI.btnSend.setEnabled(False)
+        MainWindowGUI.etxtMsg.setEnabled(False)
+        pass    
+        
 
 
+#Connect SerialPort with Port
+def ConnectWithPort(Serial_Port, PortName):
+    if Serial_Port.is_open:
+        Serial_Port.close()
+    try:
+        serialPort.setPort(PortName)
+        Serial_Port.open()
+        return True
+        print("Connected")
+    except (OSError , serial.SerialException):
+        print(serial.SerialException)
+        return False
+        pass
 
-def ConnectClicked(self):
-    print("Connect Clicked")
+#Send Message to SerialPort
+def SendMsgWithPort(Serial_Port, Msg):
+    try:
+        Serial_Port.write(Msg.encode())
+        print(Msg)
+        return True            
+    except:
+        return False
+        pass        
 
-def SendClicked(self):
-    print("Send Clicked")    
+#Send Button Function    
+def SendButtonClicked():
+    if SendMsgWithPort(serialPort , MainWindowGUI.etxtMsg.text()):
+        MainWindowGUI.listWidget.addItem(MainWindowGUI.etxtMsg.text())
+        MainWindowGUI.etxtMsg.clear()
+        print("Send")
 
-app=QtWidgets.QApplication(sys.argv)
+#comboBox Update Function
+def ComboBoxUpdate():
+    MainWindowGUI.comboBox.clear()
+    MainWindowGUI.comboBox.addItems(serial_ports())
+    
+
+#Intialize GUI
+application = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
-ui = MyFirstGUI.Ui_MainWindow()
-ui.setupUi(MainWindow)
-ui.btnConnect.clicked.connect(ConnectClicked)
-ui.btnSend.clicked.connect(SendClicked)
-
+MainWindowGUI = SerialCommunicationGUI.Ui_MainWindow()
+MainWindowGUI.setupUi(MainWindow)
+MainWindowGUI.btnConnect.clicked.connect(ConnectButtonClicked)
+MainWindowGUI.btnSend.clicked.connect(SendButtonClicked)
+MainWindowGUI.refresh.clicked.connect(ComboBoxUpdate)
+MainWindowGUI.btnSend.setEnabled(False)
+MainWindowGUI.etxtMsg.setEnabled(False)
 MainWindow.show()
-sys.exit(app.exec_())
-
+sys.exit(application.exec_())
